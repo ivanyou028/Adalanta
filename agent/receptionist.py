58618d78d78add88bbfcde8999d2784efeef0898
Ivan You <youy@fb.com>
2023-08-25 03:53:22 -0700

    connect chat interface to receptionist agent

--- agent/receptionist.py
+++ agent/receptionist.py
@@ -2,21 +2,37 @@ from langchain.tools import StructuredTool
 from langchain import OpenAI
 from langchain.agents import initialize_agent, AgentType
 # from langchain.memory import ConversationBufferMemory
-
+from langchain.callbacks.base import BaseCallbackHandler
+from langchain.schema.agent import AgentAction, AgentFinish
+from langchain.callbacks.manager import CallbackManager
 llm = OpenAI(temperature=0)
 
 def multiplier(a: float, b: float) -> float:
     """Multiply the provided floats."""
     return a * b
 
-
 tool = StructuredTool.from_function(multiplier)
 
-Receptionist = agent_executor = initialize_agent(
-    [tool],
-    llm,
-    agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,
-    verbose=True,
-)
+class MyCustomHandler(BaseCallbackHandler):
+    def __init__(self, publish):
+        super().__init__()
+        self.publish = publish
+    
+    def on_agent_action(self, action: AgentAction, **kwargs) -> None:
+        self.publish("agent is using " + action.tool)
 
-# agent_executor.run("What is 3 times 4")
+    def on_agent_finish(self, finish: AgentFinish, **kwargs) -> None:
+        self.publish("agent is finished ")
+
+class Receptionist:
+    def __init__(self, publish):
+        self.agent = initialize_agent(
+            [tool],
+            llm,
+            agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,
+            verbose=True,
+            callback_manager=CallbackManager([MyCustomHandler(publish)])
+        )
+    
+    def run(self, query):
+        return self.agent.run(query)
--- streamlit/app.py
+++ streamlit/app.py
@@ -1,28 +1,14 @@
 import streamlit as st
 from agent import Receptionist
+import time
 
 st.title("Adalanta")
 
-# Initialize chat history
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-
-# Accept user input
-if prompt := st.chat_input("What is up?"):
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    # Display user message in chat message container
-    with st.chat_message("user"):
-        st.markdown(prompt)
-    # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        message_placeholder = st.empty()
-        full_response = Receptionist.run(prompt)
-        message_placeholder.markdown(full_response)
-        
-    st.session_state.messages.append({"role": "assistant", "content": full_response})
+if input := st.chat_input("Ask a question"):
+    hook = st.empty()
+    container = st.container()
+    def publish(str):
+        message = container.chat_message("assistant")
+        message.write(str)
+    output = Receptionist(publish).run(input)
+    publish(output)
